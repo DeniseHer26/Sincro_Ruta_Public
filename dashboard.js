0a2ccc80b615927b17d14d65764d208ae83d1f67
Luis Diaz <luisantoniodiaz97@gmail.com>
2025-11-21 06:56:17 -0600

    update: Angular framework and login screen

--- dashboard.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const storedUsername = localStorage.getItem('username');
-
-    if (storedUsername) {
-        document.getElementById('user-name').textContent = storedUsername;
-    } else {
-        window.location.href = 'index.html';
-    }
-});
---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+// Angular Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatButtonModule,
+    MatIconModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'logipulse';
+}
