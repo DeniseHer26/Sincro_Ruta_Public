3437fc7d3107816b92922b0ab76fdf843af5f532
Luis Diaz <luisantoniodiaz97@gmail.com>
2026-01-25 20:50:47 -0600

    update: typescript version & feat: weather service

--- src/app/core/services/weather.service.ts
+++ src/app/core/services/weather.service.ts
@@ -1,4 +1,3 @@
-// src/app/core/services/weather.service.ts
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment.development';
@@ -6,10 +5,12 @@ import { environment } from '../../../environments/environment.development';
 @Injectable({ providedIn: 'root' })
 export class WeatherService {
   private http = inject(HttpClient);
-  private apiKey = environment.OPENWEATHER_API_KEY;
+  private readonly apiUrl = `${environment.apiUrl}/servicios/clima`
 
   getForecast(lat: number, lng: number) {
-    // Obtenemos el clima basado en las coordenadas del destino
-    return this.http.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${this.apiKey}&units=metric&lang=es`);
-  }
+    // Los parámetros viajan a nuestro controlador de NestJS
+    return this.http.get(this.apiUrl, {
+      params: { lat: lat.toString(), lon: lng.toString()}
+  })
+}
 }
